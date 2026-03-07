538027491d898703d740dcb45eb13beb765526a4
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-07 03:11:14 +0200

    Add files via upload

--- driver/database/__init__.py
+++ driver/database/__init__.py
@@ -1,4 +1,4 @@
 """ database copyright """
 
 # thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
-# implemented by levina-lab (https://github.com/I0_I6)
+# implemented by levina-lab (https://github.com/ScolzeWA)
