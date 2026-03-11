949eeda10f5f274d0e8e14bd18ed5e217242918d
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-11 07:42:58 +0200

    Add files via upload

--- driver/veez.py
+++ driver/veez.py
@@ -1,4 +1,5 @@
 from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
+from os import getenv
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
@@ -10,10 +11,20 @@ bot = Client(
     plugins={"root": "program"},
 )
 
-user = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)
+# Support SESSION_STRING (pyrogram string session) or SESSION_NAME (file-based)
+SESSION_STRING = getenv("SESSION_STRING", "")
+if SESSION_STRING:
+    user = Client(
+        "veez_user",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION_STRING,
+    )
+else:
+    user = Client(
+        SESSION_NAME,
+        api_id=API_ID,
+        api_hash=API_HASH,
+    )
 
 call_py = PyTgCalls(user)
