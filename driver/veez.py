f89c062319dbdfba02bb800b125ac197e7698e1f
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-11 12:50:56 +0200

    Update veez.py

--- driver/veez.py
+++ driver/veez.py
@@ -1,6 +1,9 @@
 from config import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 from pyrogram import Client
 from pytgcalls import PyTgCalls
+import os
+
+SESSION_STRING = os.getenv("SESSION_STRING", "")
 
 bot = Client(
     ":veez:",
@@ -10,10 +13,18 @@ bot = Client(
     plugins={"root": "program"},
 )
 
-user = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH,
-)
+if SESSION_STRING:
+    user = Client(
+        SESSION_NAME,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        session_string=SESSION_STRING,
+    )
+else:
+    user = Client(
+        SESSION_NAME,
+        api_id=API_ID,
+        api_hash=API_HASH,
+    )
 
 call_py = PyTgCalls(user)
