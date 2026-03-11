0a4ca454c2533b4f581e47a08be8dbd8de482695
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-11 11:19:21 +0200

    Add files via upload

--- driver/admins.py
+++ driver/admins.py
@@ -1,6 +1,8 @@
+from pyrogram.enums import ChatMembersFilter
 from typing import List
 from pyrogram.types import Chat
 from cache.admins import get as gett, set
+
 
 async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
@@ -8,12 +10,10 @@ async def get_administrators(chat: Chat) -> List[int]:
     if get:
         return get
     else:
-        administrators = await chat.get_members(filter="administrators")
-        to_set = []
+        administrators = []
+        async for member in chat.get_members(filter=ChatMembersFilter.ADMINISTRATORS):
+            if member.privileges and member.privileges.can_manage_video_chats:
+                administrators.append(member.user.id)
 
-        for administrator in administrators:
-            if administrator.can_manage_voice_chats:
-                to_set.append(administrator.user.id)
-
-        set(chat.id, to_set)
-        return await get_administrators(chat)
+        set(chat.id, administrators)
+        return administrators
