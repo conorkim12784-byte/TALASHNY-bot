51d8c1edbd60f14f21bdf5797a290185fd1bc2e0
conorkim12784-byte <conorkim12784@gmail.com>
2026-03-15 20:13:18 +0200

    Add files via upload

--- program/ar_download2.py
+++ program/ar_download2.py
@@ -78,6 +78,7 @@ async def song(_, message: Message):
 
 
 @Client.on_message(command2(["تحميل_فيديو", "تحميل فيديو"]))
+async def vsong(client, message: Message):
     await message.delete()
     ydl_opts = {
         "format": "bestvideo[height<=720]+bestaudio/best",
